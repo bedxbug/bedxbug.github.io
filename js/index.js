a6954e018ba6bbe38b6e57219070bef490af1eee
arifsuhan <suhan10chowdhury@gmail.com>
2019-05-13 00:38:32 +0600

    without button

--- js/index.js
+++ js/index.js
@@ -18,23 +18,33 @@ var data = document.getElementById("dataList");
 var button = document.getElementById('button1');
 var row_length = data.rows.length - 1;
 var cell_length = data.rows[0].cells.length ;
-button.addEventListener("click",run_data);
+//button.addEventListener("click",run_data);
 
 // console.log(json_length);
 
-
-function run_data()
-{
-	for(var j = 1; j < json_length ; j++)
-	{	
-		var row = data.insertRow(j);
-		for(var i=0; i<keys.length; i++){
-			var key = keys[i];
-			var temp = row.insertCell(i);
-			temp.innerHTML = json_obj.courses[j][key];
-		}
+for(var j = 1; j < json_length ; j++)
+{	
+	var row = data.insertRow(j);
+	for(var i=0; i<keys.length; i++){
+		var key = keys[i];
+		var temp = row.insertCell(i);
+		temp.innerHTML = json_obj.courses[j][key];
 	}
 }
+
+
+// function run_data()
+// {
+// 	for(var j = 1; j < json_length ; j++)
+// 	{	
+// 		var row = data.insertRow(j);
+// 		for(var i=0; i<keys.length; i++){
+// 			var key = keys[i];
+// 			var temp = row.insertCell(i);
+// 			temp.innerHTML = json_obj.courses[j][key];
+// 		}
+// 	}
+// }
 
 
 
